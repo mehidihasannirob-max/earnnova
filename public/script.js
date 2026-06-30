01a01b011d1c27fef552064b76e0c3f2645faf82
mehidihasannirob-max <mehidihasannirob@gmail.com>
2026-06-30 22:10:43 +0600

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("button");
+
+  buttons[0].addEventListener("click", () => {
+    alert("Telegram Login will be added soon.");
+  });
+
+  buttons[1].addEventListener("click", () => {
+    alert("Start Earning feature is under development.");
+  });
+});
